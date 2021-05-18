7030347956d211c23b41681be1b2af86bea07358
Jibril Mohamed <41453767+jabrayare@users.noreply.github.com>
2021-05-17 23:09:54 -0400

    Will add the settings.py file to gitignore file

--- twitterClone/settings.py
+++ twitterClone/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = 'c0f-qf63#@1%l3tke4j57p(^8c+$#nxw^fedv)2ux2t4lvehbp'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
